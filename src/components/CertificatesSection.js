7962dca3d76afc4e3f441d9f600498600a67b69b
Swastik Shetty <swastikshetty06ss@gmail.com>
2025-07-29 15:23:01 +0530

    . git/COMMIT_EDITMSG

--- src/components/CertificatesSection.js
+++ src/components/CertificatesSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import './CertificatesSection.css';
-
+ 
 function CertificatesSection() {
   const [ref, inView] = useInView({
     threshold: 0.3,
